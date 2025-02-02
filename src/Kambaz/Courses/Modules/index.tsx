490f48e4c4bfcae6402fcecbb426efff02b55506
Tina <y634huan@uwaterloo.ca>
2025-02-02 12:45:41 -0500

    Added components; custom implementation for A2 is still pending.

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -1,85 +1,124 @@
+import { BsGripVertical } from "react-icons/bs";
+import ModulesControls from "./ModulesControls";
+import ModuleControlButtons from "./ModuleControlButtons";
+import LessonControlButtons from "./LessonControlButtons";
 export default function Modules() {
   return (
     <div>
-      <button>Collapse All</button> <button>View Progress</button>
-      <select id="wd-publish">
-        <option value="Publish One">Publish One</option>
-        <option selected value="Publish All">
-          Publish All
-        </option>
-      </select>
-      <button>+ Module</button>
-      <ul id="wd-modules">
-        <li className="wd-module">
-          <div className="wd-title">
+      <ModulesControls />
+      <br />
+      <br />
+      <br />
+      <br />
+      <ul id="wd-modules" className="list-group rounded-0">
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" />
             Week 1, Lecture 1 - Course Introduction, Syllabus
+            <ModuleControlButtons />
           </div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">
-                  Learn what is Web Development
-                </li>
-              </ul>
-              <span className="wd-title">READING</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+              LEARNING OBJECTIVES
+              <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+              Introduction to the course
+              <LessonControlButtons />
+            </li>
+
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+              Learn what is Web Development
+              <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+              LESSON 1
+              <LessonControlButtons />
+            </li>
+
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+              LESSON 2
+              <LessonControlButtons />
+            </li>
+
+            {/* <span className="wd-title">READING</span>
+              <ul className="wd-content list-group rounded-0">
+                <li className="wd-lessons list-group-item p-3 ps-1">
                   Full Stack Developer - Chapter 1 - Introduction
                 </li>
-                <li className="wd-content-item">
+                <li className="wd-lessons list-group-item p-3 ps-1">
                   Full Stack Developer - Chapter 2 - User Interface
                 </li>
               </ul>
               <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
+              <ul className="wd-content list-group rounded-0">
+                <li className="wd-lessons list-group-item p-3 ps-1">
                   Introduction to Web Development
                 </li>
-                <li className="wd-content-item">
+                <li className="wd-lessons list-group-item p-3 ps-1">
                   Creating an HTTP Server with NodeJS
                 </li>
-                <li className="wd-content-item">
+                <li className="wd-lessons list-group-item p-3 ps-1">
                   Creating a React Applications
                 </li>
-              </ul>
-            </li>
+              </ul> */}
           </ul>
         </li>
-        <li className="wd-module">
-          <div className="wd-title">
+        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" />
             Week 2, Lecture 2 - Formatting User Interface with HTML
+            <ModuleControlButtons />
           </div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
+          <ul className="wd-lessons list-group rounded-0">
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
               <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
+              <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+              <span className="wd-title">LESSON 1</span>
+              <LessonControlButtons />
+            </li>
+            <li className="wd-lesson list-group-item p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" />
+              <span className="wd-title">LESSON 2</span>
+              <LessonControlButtons />
+            </li>
+            {/* <li className="wd-lesson list-group-item p-3 ps-1">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content list-group rounded-0">
+                <li className="wd-lesson list-group-item p-3 ps-1">
                   Learn how to create user interface with HTML
                 </li>
-                <li className="wd-content-item">
+                <li className="wd-lesson list-group-item p-3 ps-1">
                   Deploy the assignment to Netlify
                 </li>
               </ul>
               <span className="wd-title">SLIDES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">
+              <ul className="wd-content list-group rounded-0">
+                <li className="wd-lesson list-group-item p-3 ps-1">
                   Introduction to HTML and DOM
                 </li>
-                <li className="wd-content-item">
+                <li className="wd-lesson list-group-item p-3 ps-1">
                   Formatting web content with headings
                 </li>
-                <li className="wd-content-item">
+                <li className="wd-lesson list-group-item p-3 ps-1">
                   Formatting content with lists and tables
                 </li>
               </ul>
-            </li>
+            </li> */}
           </ul>
         </li>
-        <li className="wd-module">
+        {/* <li className="wd-module">
           <div className="wd-title">Week 3</div>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -4,111 +4,141 @@ export default function Dashboard() {
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <h5> CS1234 React JS </h5>
-            <div>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <img src="/images/reactjs.jpg" width="100%" height={160} />
+                <div className="card-body d-flex flex-column flex-grow-1">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS1234 React JS
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Full Stack software developer
+                  </p>
+                  <button className="btn btn-primary mt-auto"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <h5> CS6662 Program Design Paradigms </h5>
-            <div>
-              <p className="wd-dashboard-course-title">
-                Object-oriented design and functional design
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <img src="/images/reactjs.jpg" width="100%" height={160} />
+                <div className="card-body d-flex flex-column flex-grow-1">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS6662 Program Design Paradigms
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    OOD and functional design
+                  </p>
+                  <button className="btn btn-primary mt-auto"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <h5> CS2744 Computer Systems </h5>
-            <div>
-              <p className="wd-dashboard-course-title">
-                Different operating systems
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <img src="/images/reactjs.jpg" width="100%" height={160} />
+                <div className="card-body d-flex flex-column flex-grow-1">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS2744 Computer Systems
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Different operating systems
+                  </p>
+                  <button className="btn btn-primary mt-auto"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <h5> CS3066 Web Development </h5>
-            <div>
-              <p className="wd-dashboard-course-title">
-                Front-end and back-end frameworks
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <img src="/images/reactjs.jpg" width="100%" height={160} />
+                <div className="card-body d-flex flex-column flex-grow-1">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS3066 Web Development
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Front-end and back-end frameworks
+                  </p>
+                  <button className="btn btn-primary mt-auto"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <h5> CS7777 Database </h5>
-            <div>
-              <p className="wd-dashboard-course-title">
-                Design data intensive applications
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <img src="/images/reactjs.jpg" width="100%" height={160} />
+                <div className="card-body d-flex flex-column flex-grow-1">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS7777 Database
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Design data intensive applications
+                  </p>
+                  <button className="btn btn-primary mt-auto"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <h5> CS7889 Securities </h5>
-            <div>
-              <p className="wd-dashboard-course-title">
-                Discuss security topics
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <img src="/images/reactjs.jpg" width="100%" height={160} />
+                <div className="card-body d-flex flex-column flex-grow-1">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS7889 Securities
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Discuss security topics
+                  </p>
+                  <button className="btn btn-primary mt-auto"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link
-            to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link"
-          >
-            <img src="/images/reactjs.jpg" width={200} />
-            <h5> CS4022 Graphical User Interface</h5>
-            <div>
-              <p className="wd-dashboard-course-title">
-                Design application interfaces
-              </p>
-              <button> Go </button>
+          </div>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+                to="/Kambaz/Courses/1234/Home"
+              >
+                <img src="/images/reactjs.jpg" width="100%" height={160} />
+                <div className="card-body d-flex flex-column flex-grow-1">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS4022 Graphical User Interface
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Design application interfaces
+                  </p>
+                  <button className="btn btn-primary mt-auto"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
+          </div>
         </div>
       </div>
     </div>
