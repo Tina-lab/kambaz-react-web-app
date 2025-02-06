312381255072ea2bb1e6fc8fd0ddc025e4193d22
Tina <y634huan@uwaterloo.ca>
2025-02-05 21:41:20 -0500

    Added full name and section number

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,9 @@ export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
+      Full Name: Yuqing Huang
+      <br />
+      Section:01
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
