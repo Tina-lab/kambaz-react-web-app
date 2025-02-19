23e7d140797d2b779629add0c0e0a399f19768c7
Tina <y634huan@uwaterloo.ca>
2025-02-19 18:53:47 -0500

    Update minor syntaxes for Lab3

--- src/Labs/Lab3/Square.tsx
+++ src/Labs/Lab3/Square.tsx
@@ -3,6 +3,7 @@ export default function Square({ children }: { children: ReactNode }) {
   const num = Number(children);
   return (
     <div>
+      <h4>Square of 4</h4>
       <span id="wd-square">{num * num}</span>
       <hr />
     </div>
