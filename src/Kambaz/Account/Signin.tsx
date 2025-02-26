723900d2ed007218c4089be2578f09a6069b7558
Tina <y634huan@uwaterloo.ca>
2025-02-25 23:27:14 -0500

    Finished up until 4.4.4.2

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -1,21 +1,51 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { setCurrentUser } from "./reducer";
+import { useDispatch } from "react-redux";
+import * as db from "../Database";
 export default function Signin() {
+  const [credentials, setCredentials] = useState<any>({});
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const signin = () => {
+    const user = db.users.find(
+      (u: any) =>
+        u.username === credentials.username &&
+        u.password === credentials.password
+    );
+    if (!user) return;
+    dispatch(setCurrentUser(user));
+    navigate("/Kambaz/Dashboard");
+  };
   return (
     <div id="wd-signin-screen">
       <h3>Sign in</h3>
-      <input placeholder="username" className="wd-username form-control mb-2" />
+      <input
+        placeholder="username"
+        className="wd-username form-control mb-2"
+        defaultValue={credentials.username}
+        onChange={(e) =>
+          setCredentials({ ...credentials, username: e.target.value })
+        }
+        id="wd-username"
+      />
       <input
         placeholder="password"
         type="password"
         className="wd-password form-control mb-2"
+        defaultValue={credentials.password}
+        onChange={(e) =>
+          setCredentials({ ...credentials, password: e.target.value })
+        }
+        id="wd-password"
       />
-      <Link
-        to="/Kambaz/Dashboard"
+      <button
+        onClick={signin}
         id="wd-signin-btn"
         className="btn btn-primary w-100"
       >
         Sign in
-      </Link>
+      </button>
       <Link to="/Kambaz/Account/Signup" id="wd-signup-link">
         Sign up
       </Link>
--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -4,36 +4,54 @@ import ModulesControls from "./ModulesControls";
 import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 import { useParams } from "react-router";
-import * as db from "../../Database";
-import { v4 as uuidv4 } from "uuid";
+import { addModule, editModule, updateModule, deleteModule } from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
 export default function Modules() {
   const { cid } = useParams();
-  const [modules, setModules] = useState<any[]>(
-    db.modules.filter((m: any) => m.course === cid)
-  );
+  const { modules } = useSelector((state: any) => state.modulesReducer);
+  const dispatch = useDispatch();
   const [moduleName, setModuleName] = useState("");
-  const addModule = () => {
-    setModules([
-      ...modules,
-      { _id: uuidv4(), name: moduleName, course: cid, lessons: [] },
-    ]);
-    setModuleName("");
-  };
+  const filteredModules = modules.filter(
+    (module: any) => module.course === cid
+  );
   return (
     <div className="d-flex flex-column">
       <ModulesControls
         setModuleName={setModuleName}
         moduleName={moduleName}
-        addModule={addModule}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }}
       />
       <br />
       <ul id="wd-modules" className="list-group rounded-0">
-        {modules.map((module) => (
+        {filteredModules.map((module: any) => (
           <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
             <div className="wd-title p-3 ps-2 bg-secondary">
               <BsGripVertical className="me-2 fs-3" />
-              {module.name}
-              <ModuleControlButtons />
+              {!module.editing && module.name}
+              {module.editing && (
+                <input
+                  className="form-control w-50 d-inline-block"
+                  onChange={(e) =>
+                    dispatch(updateModule({ ...module, name: e.target.value }))
+                  }
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      dispatch(updateModule({ ...module, editing: false }));
+                    }
+                  }}
+                  defaultValue={module.name}
+                />
+              )}
+              <ModuleControlButtons
+                moduleId={module._id}
+                deleteModule={(moduleId) => {
+                  dispatch(deleteModule(moduleId));
+                }}
+                editModule={(moduleId) => dispatch(editModule(moduleId))}
+              />
             </div>
             {module.lessons && (
               <ul className="wd-lessons list-group rounded-0">
