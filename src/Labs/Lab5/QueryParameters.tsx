1b66af6095e48411ae66921e8ad8d5f9e1fcc913
Tina <y634huan@uwaterloo.ca>
2025-03-29 13:32:57 -0400

    Finished up until page 157

--- src/Labs/Lab5/QueryParameters.tsx
+++ src/Labs/Lab5/QueryParameters.tsx
@@ -1,67 +1,66 @@
 import { useState } from "react";
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
-export default function PathParameters() {
+export default function QueryParameters() {
   const [a, setA] = useState("34");
   const [b, setB] = useState("23");
   return (
-    <div>
-      <h3>Path Parameters</h3>
+    <div id="wd-query-parameters">
+      <h3>Query Parameters</h3>
       <input
+        id="wd-query-parameter-a"
         className="form-control mb-2"
-        id="wd-path-parameter-a"
-        type="number"
         defaultValue={a}
+        type="number"
         onChange={(e) => setA(e.target.value)}
       />
       <input
+        id="wd-query-parameter-b"
         className="form-control mb-2"
-        id="wd-path-parameter-b"
-        type="number"
         defaultValue={b}
+        type="number"
         onChange={(e) => setB(e.target.value)}
       />
       <a
+        id="wd-query-parameter-add"
         className="btn btn-primary me-2"
-        id="wd-path-parameter-add"
-        href={`${REMOTE_SERVER}/lab5/add/${a}/${b}`}
+        href={`${REMOTE_SERVER}/lab5/calculator?operation=add&a=${a}&b=${b}`}
       >
         Add {a} + {b}
       </a>
       <a
         className="btn btn-danger"
-        id="wd-path-parameter-subtract"
-        href={`${REMOTE_SERVER}/lab5/subtract/${a}/${b}`}
+        id="wd-query-parameter-subtract"
+        href={`${REMOTE_SERVER}/lab5/calculator?operation=subtract&a=${a}&b=${b}`}
       >
         Substract {a} - {b}
       </a>
-      <hr />
       <input
+        id="wd-query-parameter-a"
         className="form-control mb-2"
-        id="wd-path-parameter-a"
-        type="number"
         defaultValue={a}
+        type="number"
         onChange={(e) => setA(e.target.value)}
       />
       <input
+        id="wd-query-parameter-b"
         className="form-control mb-2"
-        id="wd-path-parameter-b"
-        type="number"
         defaultValue={b}
+        type="number"
         onChange={(e) => setB(e.target.value)}
       />
       <a
+        id="wd-query-parameter-add"
         className="btn btn-primary me-2"
-        id="wd-path-parameter-add"
-        href={`${REMOTE_SERVER}/lab5/multiply/${a}/${b}`}
+        href={`${REMOTE_SERVER}/lab5/calculator?operation=multiply&a=${a}&b=${b}`}
       >
-        multiply {a} * {b}
+        Multiply {a} + {b}
       </a>
       <a
         className="btn btn-danger"
-        id="wd-path-parameter-subtract"
-        href={`${REMOTE_SERVER}/lab5/divide/${a}/${b}`}
+        id="wd-query-parameter-subtract"
+        href={`${REMOTE_SERVER}/lab5/calculator?operation=divide&a=${a}&b=${b}`}
       >
-        divide {a} / {b}
+        Divide {a} - {b}
       </a>
       <hr />
     </div>
