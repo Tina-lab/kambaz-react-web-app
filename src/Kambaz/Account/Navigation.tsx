dc3c65a6a782fb550ea845a40143b4d298125379
Tina <y634huan@uwaterloo.ca>
2025-04-07 12:22:15 -0400

    Updated navigation page to better reflect where we are.

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
-
+  const { pathname } = useLocation();
+  const active = (path: string) =>
+    pathname.includes(path) ? "active text-dark" : "";
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
       {!currentUser && (
@@ -11,14 +13,18 @@ export default function AccountNavigation() {
           <Link
             to="/Kambaz/Account/Signin"
             id="wd-account-signin-link"
-            className="list-group-item active border border-0"
+            className={`list-group-item text-danger ${active(
+              "Signin"
+            )} border border-0`}
           >
             Signin
           </Link>
           <Link
             to="/Kambaz/Account/Signup"
             id="wd-account-signup-link"
-            className="list-group-item text-danger border border-0"
+            className={`list-group-item text-danger ${active(
+              "Signup"
+            )} border border-0`}
           >
             Signup
           </Link>
